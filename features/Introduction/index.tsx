17b9ad25417637f0291c3fbf805e3b75208d6cbd
Petr Brantalík <grounipteam@gmail.com>
2023-01-29 16:43:59 +0100

    feat: RA basics

---
+++ features/Introduction/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import { View, Text } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSpring,
+  withRepeat,
+} from "react-native-reanimated";
+import { styles } from "./styles";
+
+const SIZE = 100;
+
+const handleRotation = (progress: Animated.SharedValue<number>) => {
+  "worklet";
+  return `${progress.value * 2 * Math.PI}rad`;
+};
+export const Introduction = () => {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      borderRadius: (progress.value * SIZE) / 2,
+      transform: [{ scale: scale.value }, { rotate: handleRotation(progress) }],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), Infinity, true); // Infinite loop or we can use -1 value
+    scale.value = withRepeat(withSpring(1), Infinity, true);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          { height: SIZE, width: SIZE, backgroundColor: "blue" },
+          reanimatedStyle,
+        ]}
+      />
+    </View>
+  );
+};
