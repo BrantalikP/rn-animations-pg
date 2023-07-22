aeddce75218c7acbe92ca4e723727f2188113412
Petr Brantalík <grounipteam@gmail.com>
2023-07-22 08:46:34 +0200

    fix: TS

--- src/features/animations/AnimatedList/AnimatedList.tsx
+++ src/features/animations/AnimatedList/AnimatedList.tsx
@@ -1,14 +1,27 @@
 import { View, Text, Animated, FlatList, StatusBar } from "react-native";
 import { styles } from "./styles";
 import { Item } from "./components/Item";
+import {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 
 interface IAnimatedList {
   data: { key: string; color: string; height: number }[];
 }
 
-const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+const AnimatedFlatList = Animated.createAnimatedComponent(
+  FlatList<IAnimatedList["data"][number]>
+);
 
 const AnimatedList = ({ data }: IAnimatedList) => {
+  const scrollY = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler((event) => {
+    "worklet";
+    // Update the scrollY value with the current scroll offset
+    scrollY.value = event.contentOffset.y;
+  });
   return (
     <View style={styles.container}>
       <StatusBar hidden />
@@ -19,7 +32,7 @@ const AnimatedList = ({ data }: IAnimatedList) => {
         scrollEventThrottle={16}
         // CellRendererComponent={CustomCellRendererComponent}
         renderItem={({ item, index }) => {
-          return <Item />;
+          return <Item item={item} index={index} scrollY={scrollY} />;
         }}
       />
     </View>
--- src/features/animations/AnimatedList/components/Item/Item.tsx
+++ src/features/animations/AnimatedList/components/Item/Item.tsx
@@ -1,14 +1,42 @@
-import { View, Text } from 'react-native'
-import { styles } from './styles'
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+import Animated, { useAnimatedStyle } from "react-native-reanimated";
+import { SPACING } from "../../styles";
 
-interface IItem {}
-
-const Item = ({}: IItem) => {
-  return (
-    <View style={styles.container}>
-      <Text>Item</Text>
-    </View>
-  )
+interface IItem {
+  item: {
+    key: string;
+    color: string;
+    height: number;
+  };
+  index: number;
+  scrollY: Animated.SharedValue<number>;
 }
 
-export { Item }
+const Item = ({ item, scrollY, itemY, itemHeight }: IItem) => {
+  console.log({ itemY, itemHeight });
+  const stylez = useAnimatedStyle(() => {
+    return {};
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          backgroundColor: item.color,
+          marginBottom: SPACING,
+          height: item.height,
+          padding: SPACING,
+          borderRadius: 16,
+          justifyContent: "flex-end",
+          alignItems: "flex-end",
+        },
+        stylez,
+      ]}
+    >
+      <Text>{item.color}</Text>
+    </Animated.View>
+  );
+};
+
+export { Item };
