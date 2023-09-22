f27e76dbb92b58f370e17eb653d60ec91941f080
Petr Brantalík <grounipteam@gmail.com>
2023-09-22 09:40:51 +0200

    feat: added useDrawing hook

--- src/features/animations/FourierVisualizer/index.tsx
+++ src/features/animations/FourierVisualizer/index.tsx
@@ -1,9 +1,28 @@
+import { Canvas, Path } from "@shopify/react-native-skia";
 import { Text, View } from "react-native";
+import {
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated from "react-native-reanimated";
 import { styles } from "./styles";
+import { useDrawing } from "./useDrawing";
 export const FourierVisualizer = () => {
+  const { drawPath, opacity, panGesture } = useDrawing();
   return (
-    <View style={styles.container}>
-      <Text>Fourier Visualizer</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={panGesture}>
+        <Animated.View style={styles.container}>
+          <Canvas style={styles.canvas}>
+            <Path
+              path={drawPath}
+              strokeWidth={10}
+              style={"stroke"}
+              opacity={opacity}
+            />
+          </Canvas>
+        </Animated.View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 };
