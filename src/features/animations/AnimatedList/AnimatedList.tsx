84a0f67280728959f38a85b3b0d237092734b1e1
Petr Konecny <petr.konecny@strv.com>
2023-10-06 11:28:04 +0200

    fix: fixed the calculation

--- src/features/animations/AnimatedList/AnimatedList.tsx
+++ src/features/animations/AnimatedList/AnimatedList.tsx
@@ -1,12 +1,11 @@
-import { View, Text, FlatList, StatusBar, Dimensions } from "react-native";
+import { View, FlatList, StatusBar, Dimensions } from "react-native";
 import { styles } from "./styles";
 import { Item } from "./components/Item";
 import Animated, {
   useAnimatedScrollHandler,
   useSharedValue,
 } from "react-native-reanimated";
-import { CustomCellRendererComponent } from "./components/CustomCellRendererComponent";
-import { DataIds, data } from "@/features/home/screens/presets";
+import { data } from "@/features/home/screens/presets";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 interface IAnimatedList {
@@ -31,24 +30,15 @@ const AnimatedList = ({ data }: IAnimatedList) => {
       <AnimatedFlatList
         data={data}
         keyExtractor={(item) => item.id}
-        style={{ marginLeft: -width / 8.5, marginTop: -height / 4 }}
-        numColumns={2}
-        contentContainerStyle={{ paddingBottom: height / 4 }}
-        getItemLayout={(data, index) => {
-          return {
-            length: height / 2,
-            offset: height / 2 * index,
-            index,
-          };
-        }}
         onScroll={onScroll}
+        numColumns={2}
         scrollEventThrottle={16}
         renderItem={({ item, index }) => {
           return (
-            <View style={{ width: width / 2, height: height / 2 }}>
-              <View style={{ transform: [{ scale: 0.47 }] }}>
+            <View style={{ width: width / 2, height: height / 2}}>
+              <Animated.View style={{ transform: [{ scale: 0.47 }, { translateX: -width / 4}, { translateY: -height / 2 }] }}>
                 <Item item={item} index={index} scrollY={scrollY} />
-              </View>
+              </Animated.View>
             </View>
           );
         }}
