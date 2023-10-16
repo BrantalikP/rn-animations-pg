21b45ac3db84f5c33c60e6e1336e07177406fd2f
Petr Konecny <petr.konecny@strv.com>
2023-10-16 17:42:41 +0200

    feat(splashScreen): animation rewritten into Skia

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Provider } from "@/provider";
 import { theme } from "@/theme";
 import { Stack } from "expo-router";
-import Color from 'color'
+import Color from "color";
 import MaskedView from "@react-native-masked-view/masked-view";
 import React, { useEffect } from "react";
 import { View, Text, Dimensions, StyleSheet } from "react-native";
@@ -16,13 +16,27 @@ import Animated, {
   Easing,
   Extrapolate,
   FadeInDown,
+  useDerivedValue,
 } from "react-native-reanimated";
-import * as SystemUI from 'expo-system-ui';
+import * as SystemUI from "expo-system-ui";
 import Svg, { Path } from "react-native-svg";
+import {
+  Canvas,
+  Circle,
+  Fill,
+  Group,
+  Rect,
+  Skia,
+  Text as SkiaText,
+  useComputedValue,
+  useFont,
+} from "@shopify/react-native-skia";
 
 const { width, height } = Dimensions.get("window");
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
+
+const center = { x: width / 2, y: height / 2 };
 
 SystemUI.setBackgroundColorAsync("black");
 
@@ -41,7 +55,8 @@ const CircleTransitionMask = ({
 }) => {
   const radius = useSharedValue(0); // Starting radius
 
-  const expandedRadius = Math.sqrt(width * width + height * height) / 2;
+  // const expandedRadius = Math.sqrt(width * width + height * height) / 2 + 35;
+  const expandedRadius = height / 2
 
   useEffect(() => {
     if (!enabled) return;
@@ -117,6 +132,19 @@ const CircleTransitionMask = ({
     };
   });
 
+  const textTransform = useDerivedValue(() => {
+    return [
+      {
+        scale: interpolate(
+          radius.value,
+          [0, expandedRadius],
+          [1, 22],
+          Extrapolate.CLAMP
+        ),
+      },
+    ]
+  })
+
   const animatedStyle2 = useAnimatedStyle(() => {
     return {
       transform: [
@@ -132,24 +160,55 @@ const CircleTransitionMask = ({
     };
   });
 
+  const size = 256;
+  const r = size * 0.33;
+
+  const font = useFont(require("../assets/fonts/Roboto-Bold.ttf"), 100);
+  const textWidth = font?.getTextWidth("SHOW") || 0;
+  const shWidth = font?.getTextWidth("SH") || 0;
+  const oWidth = font?.getTextWidth("O") || 0;
+  const textX = center.x - textWidth / 2;
+  const oCenter = textX + shWidth + oWidth / 2;
+  const offset = oCenter - center.x;
+
+  const clipPath = useDerivedValue(() => {
+    const path = Skia.Path.Make();
+    const width = radius.value * 2;
+    const height = radius.value * 2.9;
+    path.addOval({width, height, x: center.x - width / 2, y: center.y - height / 2 - 35});
+    return path;
+  }, [radius.value]);
+
   return (
     <>
       <View
         style={[StyleSheet.absoluteFill, { zIndex: 1000 }]}
         pointerEvents="none"
       >
-        <MaskedView
-          style={{ flex: 1 }}
-          maskElement={
-            <Svg width={width} height={height}>
-              <AnimatedPath d="" fill="black" animatedProps={animatedProps} />
-            </Svg>
-          }
-        >
-          <Animated.View style={[{ flex: 1 }, animatedStyle]}>
-            {maskedElement}
-          </Animated.View>
-        </MaskedView>
+        <Canvas style={{ flex: 1 }}>
+          <Group clip={clipPath} invertClip>
+            <Fill color="#000000" />
+            <Group
+              transform={textTransform}
+              origin={{ x: center.x, y: center.y - 35 }}
+            >
+              <SkiaText
+                x={textX - offset}
+                y={center.y}
+                text="SHOW"
+                font={font}
+                color={theme.accent1}
+              />
+              <SkiaText
+                x={textX + 50}
+                y={center.y + 80}
+                text="CASE"
+                font={font}
+                color={theme.accent1}
+              />
+            </Group>
+          </Group>
+        </Canvas>
       </View>
       <Animated.View style={[{ flex: 1 }, animatedStyle2]}>
         {children}
