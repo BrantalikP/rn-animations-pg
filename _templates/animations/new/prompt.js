6e2df8d356f59328a9a8c2df55ff2c690698b2bd
petrkonecny2 <44378486+petrkonecny2@users.noreply.github.com>
2023-10-18 10:53:45 +0200

    feat(app): added cli to generate new animations (#13)
    Co-authored-by: Petr Konecny <petr.konecny@strv.com>

---
+++ _templates/animations/new/prompt.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'animationName',
+    message: `Please enter the name of the animation.`,
+  },
+  {
+    type: 'input',
+    name: 'animationDescription',
+    message: `Please enter a description for the animation.`,
+  },
+  {
+    type: 'input',
+    name: 'animationTags',
+    message: `Please enter tags for the animation to categorize it (separated by commas).`,
+  }
+]
